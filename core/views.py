8f8b4e0e39fa9e1ef0adf8df348d4383ecf5533e
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 15:47:57 +0200

    Structure pro

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from core.models import Event, EventUser
+from core.forms import EventUserForm
 
 # Create your views here.
 
 
 def index(request):
+    event = Event.objects.get_or_create(name='Primavera Sound', number_of_places=3)[0]
+    context = {
+        'event': event,
+        'form': EventUserForm()
+    }
     return render(request, 'index.html')
--- django_primers/settings.py
+++ django_primers/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'widget_tweaks',
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -45,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
