131e4151d8446490cae3af8312e975091fd11ee1
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 15:42:48 +0200

    Add models and forms

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_places = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class EventUser(models.Model):
+    name = models.CharField(max_length=100)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='users')
+
+
+
