2c2bdcc594aef63e8075ed8c1584fc636a061d3c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 16:03:14 +0200

    Add user dynamically

--- core/views.py
+++ core/views.py
@@ -7,7 +7,15 @@ from core.forms import EventUserForm
 
 def index(request):
     event = Event.objects.get_or_create(name='Primavera Sound', number_of_places=3)[0]
-    
+    if request.method == 'POST':
+        form = EventUserForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            EventUser.objects.create(name=name, event=event)
+            context = {
+                'users': event.users.all()
+            }
+            return render(request, 'partials/userlist.html', context)
     context = {
         'event': event,
         'form': EventUserForm(),
