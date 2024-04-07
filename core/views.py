7204f0fd5dc6aa7509db712b61acc7f26fcd024c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 16:08:21 +0200

    Disable button after submit form with htmx with hyperscript

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from core.models import Event, EventUser
 from core.forms import EventUserForm
+import time
 
 # Create your views here.
 
@@ -8,6 +9,7 @@ from core.forms import EventUserForm
 def index(request):
     event = Event.objects.get_or_create(name='Primavera Sound', number_of_places=3)[0]
     if request.method == 'POST':
+        time.sleep(1)  # we added this for testing to check if hyperscript will disable the button after form submitted with htmx
         form = EventUserForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
